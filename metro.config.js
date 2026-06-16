fc98c3a61f65d5d2540b016b4ff9279de505bb8a
howery3 <aiden.howery@gmail.com>
2026-06-16 15:07:28 -0400

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,3 @@
-// EAS runs pnpm expo export:embed from the workspace root.
-// Whichever project root Metro uses, delegate to the mobile app's config.
-module.exports = require("./artifacts/mobile/metro.config.js");
+const { getDefaultConfig } = require("expo/metro-config");
+
+module.exports = getDefaultConfig(__dirname);
